648f9f2265ca8f0607da517743d81136f2c89884
Brian Tang <BRIAN.TANG.325@GMAIL.COM>
2019-05-03 15:19:07 +0700

    easy to understand merge sorted linked lists

--- src/leetcoding/MergeSortedListNodes.java
+++ src/leetcoding/MergeSortedListNodes.java
@@ -9,31 +9,26 @@ public class MergeSortedListNodes {
 	}
 	
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2){
-		ListNode resultListNode = new ListNode(0);
+		ListNode dummy = new ListNode(0);
+		ListNode temp = dummy;
 		
-		ListNode dummyHead = resultListNode;
-		
-		if(l1 == null){
-			return l2;
-		}
-		
-		if(l2 == null){
-			return l1;
-		}
-		
-		while(l1 != null || l2 != null){
-			if(l2 == null || (l1!=null && l1.val <= l2.val)){
-				dummyHead.next = new ListNode(l1.val);
-				dummyHead = dummyHead.next;
+		while(l1 != null && l2 != null){
+			if(l1.val < l2.val){
+				temp.next = l1;
 				l1 = l1.next;
 			} else {
-				dummyHead.next = new ListNode(l2.val);
-				dummyHead = dummyHead.next;
+				temp.next = l2;
 				l2 = l2.next;
 			}
 		}
 		
-		return resultListNode.next;
+		if(l1 != null){
+			temp.next = l1;
+		} else {
+			temp.next = l2;
+		}
+		
+		return dummy.next;
 		
 	}
 }
