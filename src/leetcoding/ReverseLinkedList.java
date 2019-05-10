a4e1f8013270647e7bf988df217bc19c235495f1
Brian Tang <BRIAN.TANG.325@GMAIL.COM>
2019-05-10 15:26:22 +0800

    update to linkedlist

--- src/leetcoding/ReverseLinkedList.java
+++ src/leetcoding/ReverseLinkedList.java
@@ -2,13 +2,32 @@ package leetcoding;
 
 public class ReverseLinkedList {
 	
-	private class Node {
+	public class Node {
 		int data;
 		Node next;
 		
 		Node(int d){
 			data = d;
 			next = null;
+		}
+	}
+	
+	// create a linked list
+	
+	public void reverseLinkedList(Node myNode){
+		Node head = null;
+		Node current = myNode;
+		Node previous = null;
+		
+		if(current != null && current.next == null){
+			System.out.println("Reversal done");
+		}
+		
+		while(current != null){
+			head = myNode;
+			current = head.next;
+			previous = head;
+			current.next = previous;
 		}
 	}
 }
